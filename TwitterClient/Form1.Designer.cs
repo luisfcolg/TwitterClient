a7b4c10a580d8731b3e7c0a304e709211879dd88
Luis López <luisfcolg.13@gmail.com>
2019-11-30 15:16:05 -0600

    Login page
    Added login page

--- TwitterClient/Form1.Designer.cs
+++ TwitterClient/Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loginPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.usernameLogin = new System.Windows.Forms.TextBox();
+            this.passwordLogin = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.loginPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginPanel
+            // 
+            this.loginPanel.Controls.Add(this.signupButton);
+            this.loginPanel.Controls.Add(this.loginButton);
+            this.loginPanel.Controls.Add(this.passwordLogin);
+            this.loginPanel.Controls.Add(this.usernameLogin);
+            this.loginPanel.Controls.Add(this.pictureBox1);
+            this.loginPanel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginPanel.ForeColor = System.Drawing.Color.White;
+            this.loginPanel.Location = new System.Drawing.Point(12, 12);
+            this.loginPanel.Name = "loginPanel";
+            this.loginPanel.Size = new System.Drawing.Size(989, 523);
+            this.loginPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TwitterClient.Properties.Resources.inicio;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(526, 517);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // usernameLogin
+            // 
+            this.usernameLogin.BackColor = System.Drawing.Color.Black;
+            this.usernameLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameLogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLogin.ForeColor = System.Drawing.Color.White;
+            this.usernameLogin.Location = new System.Drawing.Point(557, 34);
+            this.usernameLogin.Name = "usernameLogin";
+            this.usernameLogin.Size = new System.Drawing.Size(149, 26);
+            this.usernameLogin.TabIndex = 1;
+            // 
+            // passwordLogin
+            // 
+            this.passwordLogin.BackColor = System.Drawing.Color.Black;
+            this.passwordLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordLogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLogin.ForeColor = System.Drawing.Color.White;
+            this.passwordLogin.Location = new System.Drawing.Point(724, 34);
+            this.passwordLogin.Name = "passwordLogin";
+            this.passwordLogin.Size = new System.Drawing.Size(149, 26);
+            this.passwordLogin.TabIndex = 2;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(26)))));
+            this.loginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(161)))), ((int)(((byte)(242)))));
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(161)))), ((int)(((byte)(242)))));
+            this.loginButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.loginButton.Location = new System.Drawing.Point(897, 29);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 34);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Log in";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // signupButton
+            // 
+            this.signupButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(161)))), ((int)(((byte)(242)))));
+            this.signupButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.signupButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signupButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(161)))), ((int)(((byte)(242)))));
+            this.signupButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signupButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupButton.ForeColor = System.Drawing.Color.White;
+            this.signupButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.signupButton.Location = new System.Drawing.Point(643, 358);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(244, 34);
+            this.signupButton.TabIndex = 4;
+            this.signupButton.Text = "Sign up";
+            this.signupButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(1013, 547);
+            this.Controls.Add(this.loginPanel);
+            this.Name = "Form1";
+            this.Text = "Twitter";
+            this.loginPanel.ResumeLayout(false);
+            this.loginPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel loginPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox passwordLogin;
+        private System.Windows.Forms.TextBox usernameLogin;
+        private System.Windows.Forms.Button signupButton;
     }
 }
 
